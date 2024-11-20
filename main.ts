336a3b0a213e85323c5d0585b864e69d45e769bc
ken802 <s23081@hkfewwcb.edu.hk>
2024-11-20 12:59:59 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(10)
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
+    basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("time up")
+})
+input.onButtonPressed(Button.B, function () {
+    led.stopAnimation()
+})
